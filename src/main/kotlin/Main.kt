2e21f7e6055799ef3ddc938cca4e7b5c93803554
Rafa <rmoreno.cesar@gmail.com>
2018-07-22 11:51:27 -0500

    made Main runnable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,25 @@
-object Main {
+fun main(args: Array<String>) {
+    try {
+        val operation: String = args[0]
+
+        if (operation != "compile" || operation != "execute") {
+            throw Exception()
+        }
+
+        var i = 0
+        var flags = ""
+
+        while (++i < args.size && args[i][0] == '-') {
+            flags += args[i].substring(1)
+        }
+
+        if (i < args.size) {
+            val path = args[i]
+            Pascal(operation, path, flags)
+        } else {
+            throw Exception("Invalid input index")
+        }
+    } catch (ex: Exception) {
+        throw Exception("Invalid argument")
+    }
 }
