e3a743aac4da4f3b8f08fe082e6f5290d05445ef
Rafa <rmoreno.cesar@gmail.com>
2018-07-22 11:50:59 -0500

    updated .gitignore

--- src/main/kotlin/Pascal.kt
+++ src/main/kotlin/Pascal.kt
@@ -1,8 +1,8 @@
 import backend.Backend
+import backend.BackendFactory
 import frontend.FrontEndFactory
 import frontend.Parser
 import frontend.Source
-import backend.BackendFactory
 import intermediate.IntermediateCodeGenerator
 import intermediate.SymbolTable
 import java.io.BufferedReader
@@ -10,11 +10,11 @@ import java.io.FileReader
 
 class Pascal(operation: String, filePath: String, flags: String) {
 
-    lateinit var parser: Parser,
-    lateinit var source: Source,
-    lateinit var iCode: IntermediateCodeGenerator?,
-    lateinit var symbolTable: SymbolTable?,
-    lateinit var backend: Backend,
+    lateinit var parser: Parser
+    lateinit var source: Source
+    lateinit var iCode: IntermediateCodeGenerator
+    lateinit var symbolTable: SymbolTable
+    lateinit var backend: Backend
     lateinit var operation: String
 
     init {
@@ -29,44 +29,18 @@ class Pascal(operation: String, filePath: String, flags: String) {
             parser.addMessageListener(MessageListenerImpl())
 
             backend = BackendFactory.createBackend(operation)
-            backend.addMessageListener(ParserMessageListener())
+            backend.addMessageListener(MessageListenerImpl())
 
             parser.parse()
             source.close()
 
-            iCode = parser.intermediateCode
-            symbolTable = parser.symbolTable
+            iCode = parser.intermediateCode!!
+            symbolTable = parser.symbolTable!!
 
-            backend.process(iCode!!, symbolTable!!)
+            backend.process(iCode, symbolTable)
         } catch (ex: Exception) {
             print("***** Internal translator error. *****")
             ex.printStackTrace()
         }
-    }
-}
-
-fun main(args: List<String>) {
-    try {
-        val operation: String = args[0]
-
-        if (operation != "compile" || operation != "execute") {
-            throw Exception()
-        }
-
-        var i = 0
-        var flags = ""
-
-        while (++i < args.size && args[i][0] == '-') {
-            flags += args[i].substring(1)
-        }
-
-        if (i < args.size) {
-            val path = args[i]
-            Pascal(operation, path, flags)
-        } else {
-            throw Exception("Invalid input index")
-        }
-    } catch (ex: Exception) {
-        throw Exception("Invalid argument")
     }
 }
