83a5da287acb5db2b560e3e5642e4f260fbb2f1f
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:07:00 -0500

    added pascal string token implementation

--- src/main/kotlin/frontend/pascal/tokens/PascalStringToken.kt
+++ src/main/kotlin/frontend/pascal/tokens/PascalStringToken.kt
@@ -1,4 +1,43 @@
 package frontend.pascal.tokens
 
-class PascalStringToken {
+import frontend.Source
+import frontend.Source.Companion.EOF
+import frontend.pascal.PascalErrorCode
+
+
+class PascalStringToken(source: Source): PascalToken(source) {
+    private fun extract() {
+        val textBuffer = StringBuilder()
+        val valueBuffer = StringBuilder()
+
+        var currentChar = nextChar()
+        textBuffer.append('\'')
+
+        do {
+            if (currentChar.isWhitespace()) {
+                currentChar = ' '
+            }
+
+            if (currentChar == '\'') {
+                while ((currentChar == '\'') && peekChar() == '\'') {
+                    textBuffer.append("''")
+                    nextChar()
+                    currentChar = nextChar()
+                }
+            }
+        } while ((currentChar != '\'') && (currentChar != EOF))
+
+        if (currentChar == '\'') {
+            nextChar()
+            textBuffer.append('\'')
+
+            type = PascalTokenType.STRING
+            value = valueBuffer.toString()
+        } else {
+            type = PascalTokenType.ERROR
+            value = PascalErrorCode.UNEXPECTED_EOF
+        }
+
+        text = textBuffer.toString()
+    }
 }
