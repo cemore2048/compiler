2858ae5123e4e70a08afe3b1a4498215716663f2
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:05:41 -0500

    added pascal special symbol token implementation

--- src/main/kotlin/frontend/pascal/tokens/PascalSpecialSymbolToken.kt
+++ src/main/kotlin/frontend/pascal/tokens/PascalSpecialSymbolToken.kt
@@ -1,4 +1,58 @@
 package frontend.pascal.tokens
 
-class PascalSpecialSymbolToken {
+import frontend.Source
+import frontend.Token
+import frontend.pascal.PascalErrorCode
+
+class PascalSpecialSymbolToken(source: Source): PascalToken(source) {
+
+    fun extract() {
+        var currentChar = currentChar()
+
+        text = currentChar.toString()
+
+        when(currentChar) {
+            '+', '-', '*', '/', ',',';', '\'', '=', '(', ')', '[', ']', '{', '}', '^' -> nextChar()
+            ':' -> {
+                currentChar = nextChar()
+
+                if (currentChar == '=') {
+                    text += currentChar
+                    nextChar()
+                }
+            }
+            '<' -> {
+                currentChar = nextChar()
+
+                if (currentChar == '=') {
+                    text += currentChar
+                    nextChar()
+                } else if (currentChar == '>') {
+                    text += currentChar
+                    nextChar()
+                }
+            }
+            '>' -> {
+                currentChar = nextChar()
+
+                if (currentChar == '=') {
+                    text += currentChar
+                    nextChar()
+                }
+            }
+            '.' -> {
+                currentChar = nextChar()
+
+                if (currentChar == '.') {
+                    text += currentChar
+                    nextChar()
+                }
+            }
+            else -> {
+                nextChar()
+                type = PascalTokenType.ERROR
+                value = PascalErrorCode.INVALID_CHARACTER
+            }
+        }
+    }
 }
