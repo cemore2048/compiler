83eb6a111c9c45f0f0564d6e3756e4b5ddbda347
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:07:15 -0500

    added pascal word token implementation

--- src/main/kotlin/frontend/pascal/tokens/PascalWordToken.kt
+++ src/main/kotlin/frontend/pascal/tokens/PascalWordToken.kt
@@ -1,4 +1,21 @@
 package frontend.pascal.tokens
 
-class PascalWordToken {
+import frontend.Source
+
+class PascalWordToken(source: Source) : PascalToken(source) {
+    fun extract() {
+        val textBuffer = StringBuilder()
+        var currentChar = currentChar()
+
+        while (currentChar.isLetterOrDigit()) {
+            textBuffer.append(currentChar)
+            currentChar = nextChar()
+        }
+
+        type =
+                if (PascalTokenType.RESERVED_WORDS.contains(textBuffer.toString()))
+                    PascalTokenType.valueOf(text!!.toUpperCase())
+                else
+                    PascalTokenType.IDENTIFIER
+    }
 }
