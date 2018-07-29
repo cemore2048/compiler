a706eb1b0959aedd59b74d52422135f8de2a98b9
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:06:14 -0500

    added pascal token type implementation

--- src/main/kotlin/frontend/pascal/tokens/PascalTokenType.kt
+++ src/main/kotlin/frontend/pascal/tokens/PascalTokenType.kt
@@ -2,9 +2,7 @@ package frontend.pascal.tokens
 
 import frontend.TokenType
 
-private val FIRST_RESERVED_INDEX: Int = PascalTokenType.AND.ordinal
-
-enum class PascalTokenType(identifier: String = ""): TokenType {
+enum class PascalTokenType(identifier: String = "") : TokenType {
     AND, ARRAY, BEGIN, CASE, CONST, DIV, DO, DOWNTO, ELSE, END,
     FILE, FOR, FUNCTION, GOTO, IF, IN, LABEL, MMOD, NIL, NOT, OF,
     OR, PACKED, PROCEDURE, PROGRAM, RECORD, REPEAT, SET, THEN, TO,
@@ -17,5 +15,37 @@ enum class PascalTokenType(identifier: String = ""): TokenType {
     LEFT_BRACKET("["), RIGHT_BRACKET("]"), LEFT_BRACE("{"), RIGHT_BRACE("}"),
     UP_ARROW("^"), DOT_DOT(".."),
 
-    IDENTIFIER, INTEGER, REAL, STRING, ERROR, END_OF_FILE
+    IDENTIFIER, INTEGER, REAL, STRING, ERROR, END_OF_FILE;
+
+
+    companion object {
+        //TODO this is gross and I don't even know if it works yet. come back and fix it
+        private val tokenValues = PascalTokenType.values()
+
+        val FIRST_RESERVED_INDEX = PascalTokenType.AND.ordinal
+
+        val LAST_RESERVED_INDEX = PascalTokenType.WITH.ordinal
+
+        val FIRST_SPECIAL_INDEX = PascalTokenType.PLUS.ordinal
+
+        val LAST_SPECIAL_INDEX = PascalTokenType.DOT_DOT.ordinal
+
+        var RESERVED_WORDS: Set<String> = setOf()
+            set(value) {
+                var values = mutableListOf<String>()
+                for (i in FIRST_RESERVED_INDEX until LAST_RESERVED_INDEX) {
+                    values.add(tokenValues[i].toString().toLowerCase())
+                }
+                field = values.toSet()
+            }
+
+        var SPECIAL_SYMBOLS: Set<String> = setOf()
+            set(value) {
+                var values = mutableListOf<String>()
+                for (i in FIRST_SPECIAL_INDEX until LAST_SPECIAL_INDEX) {
+                    values.add(tokenValues[i].toString().toLowerCase())
+                }
+                field = values.toSet()
+            }
+    }
 }
