88a3e24cdf5b97dbde511ee2a11317cfd0ce782e
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:05:56 -0500

    added pascal error token implementation

--- src/main/kotlin/frontend/pascal/tokens/PascalErrorToken.kt
+++ src/main/kotlin/frontend/pascal/tokens/PascalErrorToken.kt
@@ -2,8 +2,5 @@ package frontend.pascal.tokens
 
 import frontend.Source
 import frontend.pascal.PascalErrorCode
-import frontend.pascal.PascalScanner
 
-data class PascalErrorToken(val source: Source, val errorCode: PascalErrorCode, val tokenText: String, private val scanner: PascalScanner) : PascalToken(scanner) {
-
-}
+class PascalErrorToken(source: Source, val errorCode: PascalErrorCode, val tokenText: String) : PascalToken(source) {}
