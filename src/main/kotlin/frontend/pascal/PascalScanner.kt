681c58b16417d1904b413941b78054a76be8ac5d
Rafa <rmoreno.cesar@gmail.com>
2018-07-29 16:07:28 -0500

    added pascal scanner

--- src/main/kotlin/frontend/pascal/PascalScanner.kt
+++ src/main/kotlin/frontend/pascal/PascalScanner.kt
@@ -5,7 +5,7 @@ import frontend.Scanner
 import frontend.Source
 import frontend.Source.Companion.EOF
 import frontend.Token
-import frontend.pascal.tokens.PascalErrorToken
+import frontend.pascal.tokens.*
 
 class PascalScanner(source: Source) : Scanner(source) {
     @Throws
@@ -14,19 +14,16 @@ class PascalScanner(source: Source) : Scanner(source) {
         val token: Token? = currentToken()
         val currentChar = currentChar()
 
-
-        return if (currentChar == EOF) {
-            EofToken(source, END_OF_FILE)
-        } else if (currentChar.isLetter()) {
-            PascalWordToken(source)
-        } else if (currentChar.isDigit()) {
-            PascalNumberToken(source)
-        } else if (currentChar == '\'') {
-            PascalStringToken(source)
-        } else if (PascalTokenType.SPECIAL_SYMBOLS.containsKey(currentChar.toString())) {
-            PascalSpecialSymbolToken(source)
-        } else {
-            PascalErrorToken(source)
+        return when {
+            currentChar == EOF -> {
+                //EofToken(source, PascalTokenType.END_OF_FILE)
+                EofToken(source)
+            }
+            currentChar.isLetter() -> PascalWordToken(source)
+            currentChar.isDigit() -> PascalNumberToken(source)
+            currentChar == '\'' -> PascalStringToken(source)
+            PascalTokenType.SPECIAL_SYMBOLS.contains(currentChar.toString()) -> PascalSpecialSymbolToken(source)
+            else -> PascalErrorToken(source, PascalErrorCode.INVALID_CHARACTER, currentChar.toString())
         }
     }
 
