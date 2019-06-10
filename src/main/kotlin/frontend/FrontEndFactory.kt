b4b60c2d4095fb566d8f3ec27f4b0658116e72fa
Rafa <rmoreno.cesar@gmail.com>
2019-06-09 22:37:39 -0500

    Leave comment so I know where compiler might break

--- src/main/kotlin/frontend/FrontEndFactory.kt
+++ src/main/kotlin/frontend/FrontEndFactory.kt
@@ -7,6 +7,7 @@ object FrontEndFactory {
     fun createParser(language: String, type: String, source: Source): Parser {
         if (language == "pascal" && type == "top-down") {
             val scanner: Scanner = PascalScanner(source)
+            //TODO am I passing in the correct thing here?
             return PascalParserTD(scanner)
         } else if (language != "pascal") {
             throw Exception("Parser Factory: invalid language $language")
